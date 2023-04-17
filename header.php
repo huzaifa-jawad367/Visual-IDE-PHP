efaf253c7286a0ea5409c53dd44e6a77c9582504
Huzaifa Jawad <hjawad.bscs21seecs@seecs.edu.pk>
2023-04-17 20:03:34 +0500

    First Commit

---
+++ header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Header</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+
+  </head>
+  <body class="p-3 m-0 border-0 bd-example tooltip-demo">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary" style="margin: 0;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Web-based Visual IDE</a>
+    
+  </div>
+</nav>
+
+
+    <script>
+
+
+</script>
+
+  </body>
+</html>
